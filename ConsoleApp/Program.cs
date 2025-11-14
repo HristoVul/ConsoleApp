34d1e0ed1295125e37d3ab0285a66d558f740c93
hristo <hristo@mail>
2025-11-14 14:38:09 +0200

    mgmgm

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+        string name = Console.ReadLine();
+
+        Console.WriteLine(name);
     }
 }
