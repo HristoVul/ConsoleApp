f05f9402ac33026a5c5d2f4efe63faa1ada3ba49
hristo <hristo@mail>
2026-05-13 10:16:54 +0300

    bvjnbn

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,79 +1,83 @@
-﻿using ConsoleApp.Models;
+﻿using System.Text;
+using ConsoleApp.Models;
 using ConsoleApp.Ollama;
 using ConsoleApp.Storage;
 using SpotifyAPI.Web;
 
-namespace ConsoleApp;
-
-public class Program
+namespace ConsoleApp
 {
-    static async Task Main(string[] args)
+    public class Program
     {
-        var authService = new SpotifyOAuthService();
-        var credentials = new CredentialStorage();
-
-        SpotifyClient client;
-        if (await credentials.HasValidTokenAsync())
+        static async Task Main(string[] args)
         {
-            var token = await credentials.GetTokenAsync();
-            client = await authService.FromAccessToken(token!);
-        }
-        else
-        {
-            client = await authService.AuthenticateAsync();
-        }
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            var authService = new SpotifyOAuthService();
+            var credentials = new CredentialStorage();
 
-        await credentials.SaveTokenAsync(authService.CurrentAccess);
-
-        var spotifyApi = new SpotifyApiService(client);
-
-        PrivateUser profile = await spotifyApi.GetUserProfileAsync();
-        
-        var profileStore = new ProfileStorage(profile.Id);
-
-        if (!profileStore.Exists() || profileStore.LastUpdated < DateTime.Now.AddDays(-10))
-        {
-            List<FullArtist> topArtists = await spotifyApi.GetTopArtistsAsync();
-            List<FullTrack> topTracks = await spotifyApi.GetTopTracksAsync();
-            List<PlayHistoryItem> recentHistory = await spotifyApi.GetRecentHistory();
-
-            ProfileData profileData = new ProfileData
+            SpotifyClient client;
+            if (await credentials.HasValidTokenAsync())
             {
-                Artists = topArtists.Select(t => t.Name).ToArray(),
-                
-                Tracks = topTracks.Select(t => new TrackEntry(t.Name, t.Artists.First().Name, t.Album.Name, t.Href))
-                    .ToList(),
-                PlaybackHistory = recentHistory.Select(h =>
-                    new TrackEntry(h.Track.Name, h.Track.Artists.First().Name, h.Track.Album.Name, h.Track.Href)).ToList()
-            };
-
-            await profileStore.SaveKeywordReferenceAsync(profileData.ToKeywordReference());
-        }
-
-        var ollamaService = new OllamaService("phi3:latest");
-        await ollamaService.StartAsync();
-        
-        var ollamaClient = new OllamaClient(ollamaService, await profileStore.GetKeywordReferenceAsync());
-
-        Console.WriteLine($"Hey, {profile.DisplayName}, what's on your mind:");
-        
-        while (true)
-        {
-            var input = Console.ReadLine() ?? "";
-            if (input.ToLower() == "exit")
-            {
-                ollamaClient.Dispose();   
-                break;
+                var token = await credentials.GetTokenAsync();
+                client = await authService.FromAccessToken(token!);
             }
-            
-            
-            
-            var keywords = await ollamaClient.ExtractKeywords(input);
+            else
+            {
+                client = await authService.AuthenticateAsync();
+            }
 
-            Console.WriteLine("\nKeywords:");
-            Console.WriteLine(keywords);
+            await credentials.SaveTokenAsync(authService.CurrentAccess);
 
-            await ollamaClient.StreamRecommendationsAsync(input, keywords);
+            var spotifyApi = new SpotifyApiService(client);
+
+            PrivateUser profile = await spotifyApi.GetUserProfileAsync();
+        
+            var profileStore = new ProfileStorage(profile.Id);
+
+            if (!profileStore.Exists() || profileStore.LastUpdated < DateTime.Now.AddDays(-10))
+            {
+                List<FullArtist> topArtists = await spotifyApi.GetTopArtistsAsync();
+                List<FullTrack> topTracks = await spotifyApi.GetTopTracksAsync();
+                List<PlayHistoryItem> recentHistory = await spotifyApi.GetRecentHistory();
+
+                ProfileData profileData = new ProfileData
+                {
+                    Artists = topArtists.Select(t => t.Name).ToArray(),
+                
+                    Tracks = topTracks.Select(t => new TrackEntry(t.Name, t.Artists.First().Name, t.Album.Name, t.Href))
+                        .ToList(),
+                    PlaybackHistory = recentHistory.Select(h =>
+                        new TrackEntry(h.Track.Name, h.Track.Artists.First().Name, h.Track.Album.Name, h.Track.Href)).ToList()
+                };
+
+                await profileStore.SaveKeywordReferenceAsync(profileData.ToKeywordReference());
+            }
+
+            var ollamaService = new OllamaService("phi3:latest");
+            await ollamaService.StartAsync();
+        
+            var ollamaClient = new OllamaClient(ollamaService, await profileStore.GetKeywordReferenceAsync());
+
+            Console.WriteLine($"Hey, {profile.DisplayName}, what's on your mind:");
+        
+            while (true)
+            {
+                var input = Console.ReadLine() ?? "";
+                if (input.ToLower() == "exit")
+                {
+                    ollamaClient.Dispose();   
+                    break;
+                }
+            
+            
+            
+                var keywords = await ollamaClient.ExtractKeywords(input);
+
+                Console.WriteLine("\nKeywords:");
+                Console.WriteLine(keywords);
+
+                await ollamaClient.StreamRecommendationsAsync(input, keywords);
+            }
         }
     }
 }
